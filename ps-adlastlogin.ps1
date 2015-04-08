4fdc2fd141ab1146d3dd3c94ec041f9ba3c03b7f
Joseph Nguyen <joseph.b.nguyen@gmail.com>
2015-04-08 16:24:42 -0400

    Create ps-adlastlogin.ps1

---
+++ ps-adlastlogin.ps1
@@ -0,0 +1,27 @@
+$domain = "dkdc1" 
+import-module activedirectory 
+cls 
+"Domain: " + $domain 
+$samaccountname = Read-Host 'What is the User samaccountname?' 
+"Processing the checks ..." 
+$myForest = [System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest() 
+$domaincontrollers = $myforest.Sites | % { $_.Servers } | Select Name 
+$RealUserLastLogon = $null 
+$LastusedDC = $null 
+$domainsuffix = "*."+$domain 
+foreach ($DomainController in $DomainControllers)  
+{ 
+    if ($DomainController.Name -like $domainsuffix ) 
+    { 
+        $UserLastlogon = Get-ADUser -Identity $samaccountname -Properties LastLogon -Server $DomainController.Name 
+        if ($RealUserLastLogon -le [DateTime]::FromFileTime($UserLastlogon.LastLogon)) 
+        { 
+            $RealUserLastLogon = [DateTime]::FromFileTime($UserLastlogon.LastLogon) 
+            $LastusedDC =  $DomainController.Name 
+        } 
+    } 
+} 
+"The last logon occured the " + $RealUserLastLogon + "" 
+"It was done against " + $LastusedDC + "" 
+$mesage = "............." 
+$exit = Read-Host $mesage
